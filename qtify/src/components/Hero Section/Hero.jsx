fde71989ef00412dea8ad4b37e993763ca1d7a6b
Abhay Singh <78447760+Abhaysingh77@users.noreply.github.com>
2024-04-22 14:07:23 +0530

    made changes in Hero section

--- qtify/src/components/Hero Section/Hero.jsx
+++ qtify/src/components/Hero Section/Hero.jsx
@@ -1,15 +1,13 @@
-import Style from './Hero.module.css';
-import headphone from '../../assets/hero_headphones.png'
-export default function Hero () {
-    return (
-        <div className={Style.heroSection}>
-            <div className={Style.hero}>
-                <div className={Style.heroText}>
-                    <h2>100 Thousand Songs, ad free</h2>
-                    <h2>Over thousand podcasts episodes</h2>
-                </div>
-                <img src={headphone} alt="headphone" width="200vw" />
-            </div>
-        </div>
-    )
+import Style from "./Hero.module.css";
+import headphone from "../../assets/hero_headphones.png";
+export default function Hero() {
+  return (
+    <div className={Style.hero}>
+      <div className={Style.heroText}>
+        <h2>100 Thousand Songs, ad free</h2>
+        <h2>Over thousand podcasts episodes</h2>
+      </div>
+      <img src={headphone} alt="headphone" width="200vw" />
+    </div>
+  );
 }
